f4b61cd0056bc22a8dcc1df532bbb736aa8b8397
TiagoMagBorges <tiagomagborges@gmail.com>
2025-04-05 10:54:29 -0300

    first commit

---
+++ src/main/java/br/com/investitrace/investitraceapi/InvestitraceApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.investitrace.investitraceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestitraceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestitraceApiApplication.class, args);
+	}
+
+}
